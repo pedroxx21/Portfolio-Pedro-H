be7396514f1043c417132a918f31d9eea2e72eff
Pedro Henrique Ribeiro Lopes <lopesph06@gmail.com>
2025-04-14 11:45:31 -0300

    Novo

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const img = document.querySelector("#sobre img");
+    
+    img.addEventListener("click", function() {
+        img.classList.toggle("zoomed"); // Alterna entre aumentar e voltar ao tamanho original
+    });
+});
+
